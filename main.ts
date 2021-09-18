175a71340a1c15a0e42abc70feeca4ba0503dc56
dylanspin <47520114+dylanspin@users.noreply.github.com>
2021-09-18 21:04:36 +0200

    finished

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    checkEnd()
-    led.unplot(0, height)
-    if (height < 4) {
-        height = height + 1
-    } else {
-        height = 0
+    if (ended == 0) {
+        led.unplot(0, height)
+        if (height < 4) {
+            height = height + 1
+        } else {
+            height = 0
+        }
+        led.plot(0, height)
     }
-    led.plot(0, height)
 })
 function movePong () {
     if (ended == 0) {
@@ -18,12 +19,7 @@ function movePong () {
                 if (pongX < 4) {
                     pongX = pongX + 1
                 }
-                if (pongY < 4) {
-                    pongX = pongX + 1
-                }
-                if (pongY > 0) {
-                    pongX = pongX - 1
-                }
+                setHeight()
             }
         } else {
             if (pongX == 1) {
@@ -33,52 +29,61 @@ function movePong () {
                     basic.showIcon(IconNames.Skull)
                     ended = 1
                     basic.pause(2000)
+                    start()
                 }
             } else {
                 if (pongX > 0) {
                     pongX = pongX - 1
                 }
-                if (pongY < 4) {
-                    pongX = pongX + 1
-                }
-                if (pongY > 0) {
-                    pongX = pongX - 1
-                }
+                setHeight()
             }
         }
         led.plot(pongX, pongY)
         basic.pause(500)
     }
 }
-function checkEnd () {
-    if (ended == 1) {
-        basic.clearScreen()
-        start()
+function setHeight () {
+    if (up == 0) {
+        if (pongY > 0) {
+            pongY = pongY - 1
+        } else {
+            up = 1
+        }
+    } else {
+        if (pongY < 4) {
+            pongY = pongY + 1
+        } else {
+            up = 0
+        }
     }
 }
 input.onButtonPressed(Button.B, function () {
-    checkEnd()
-    led.unplot(0, height)
-    if (height > 0) {
-        height = height - 1
-    } else {
-        height = 4
+    if (ended == 0) {
+        led.unplot(0, height)
+        if (height > 0) {
+            height = height - 1
+        } else {
+            height = 4
+        }
+        led.plot(0, height)
     }
-    led.plot(0, height)
 })
 function start () {
+    basic.clearScreen()
     ended = 0
     height = 2
     led.plot(0, height)
     pongX = 1
     pongY = 2
     led.plot(pongX, pongY)
+    up = randint(0, 1)
 }
+let up = 0
 let bounce = 0
 let pongY = 0
 let pongX = 0
-let ended = 0
 let height = 0
+let ended = 0
 start()
 basic.forever(function () {
     movePong()
